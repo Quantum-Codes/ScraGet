ab8735b6289cd619c3408bd057be53d40a1ced77
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2021-12-01 12:28:55 +0000

    changing version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ScraGet",
-    version="0.1.4",
+    version="0.1.6",
     #author="QuantumCodes",
     #author_email="author@example.com",
     description="A package that makes interacting with Scratch API easier",
